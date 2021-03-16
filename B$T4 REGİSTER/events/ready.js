cbbaadd2eb8a634b147a39f9bd7780e550b25f3d
B$T4 <77292192+beT4w@users.noreply.github.com>
2021-03-16 03:23:47 +0300

    Add files via upload

---
+++ B$T4 REGİSTER/events/ready.js
@@ -0,0 +1,28 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("online");
+  var oyun = [
+    "B$T4 ❤️ REGİSTER BOT",
+    "v12 REGİSTER BOT"
+  ];
+
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], "");
+  }, 2 * 2500);
+};
